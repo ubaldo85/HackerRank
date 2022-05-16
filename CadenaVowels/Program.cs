87d6c659ff5a34c1de4c20bc48b9f3b53248b596
ubaldo perez <ubaldo.bside@coppel.com>
2022-05-16 12:56:28 -0600

    Se sube Time Conversión y CadenaVowels...

---
+++ CadenaVowels/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CadenaVowels
+{
+    /*
+       Encontrar la cadena de vocales en una cadena de letras:
+       ejemplo: asrdrdaaeeiooouuesys
+       Si existen varias regresar la más larga y sino existe regresar 0
+     */
+
+    /*Varidar Expresiones regulares.
+     * https://www.ocpsoft.org/tutorials/regular-expressions/java-visual-regex-tester/
+     * 
+     * Tutorial expresiones regulares: https://www.adictosaltrabajo.com/2015/01/29/regexsam/
+     */
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string vowels = "aeiou";
+            string cadenaVowels = "asaaeeioouuesysaeioummertetaaaaeeeeeeiouuuuuu";
+            StringBuilder sb = new StringBuilder();
+            bool bandera = true;
+            int count = 0;
+            List<string> listCadena = new List<string>();
+
+            Regex regex = new Regex("a+e+i+o+u+");
+
+            var ocurrencias = regex.Matches(cadenaVowels);
+
+            foreach (Match match in ocurrencias)
+            {
+                listCadena.Add(match.Value);
+
+            }
+
+            var result = listCadena.Max(x => x.Length);
+            var result2 = listCadena.Where(x => x.Length >= result).FirstOrDefault();
+
+
+            Console.WriteLine("La cadena de vocales mas grande es: " + result2);
+            Console.ReadLine();
+
+            //FIN
+
+
+
+            //PRIMERA VERSIÓN PARA RECUPERAR VOCALES, NO FUNCIONÓ
+            //for (int i = 0; i < cadenaVowels.Length; i++)
+            //{
+
+            //    while(bandera)
+            //    {
+            //        if(count == 4) 
+            //        {
+            //            if ('u' == cadenaVowels[i]) 
+            //            {
+            //                sb.Append(cadenaVowels[i]);
+            //                break;
+            //            }
+            //            else 
+            //            {
+            //                listCadena.Add(sb.ToString());
+            //                sb.Clear();
+            //                count = 0;
+            //                break;
+            //            }
+                    
+            //        }
+            //        if(vowels[count] == cadenaVowels[i]) 
+            //        {
+            //            bandera = true;
+            //            sb.Append(cadenaVowels[i]);
+            //            break;
+            //        }
+            //        else 
+            //        {
+            //            bandera = true;
+            //            //sb.Append(cadenaVowels[i]);
+            //            count++;
+            //            //break;
+            //        }
+
+
+            //        else 
+            //        {
+            //            bandera = true;
+            //            sb.Clear();
+            //            count = 0;
+            //            break;
+            //        }
+            //    }
+
+            //}
+
+
+        }
+    }
+}
---
+++ TimeConversion/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string timeConversion(string s)
+    {
+        string AMPM = "";
+        string hora = "";
+        string HoraFinal = "";
+
+        AMPM = s.Substring(s.Length - 2, 2);
+        hora = s.Substring(0, 2);
+
+        //Console.WriteLine(AMPM);
+
+        string HoraSinAMPM = s.Remove(s.Length - 2, 2);
+        if (AMPM == "PM")
+        {
+            if (hora != "12")
+            {
+                HoraFinal = (Convert.ToInt32(hora) + 12).ToString() + HoraSinAMPM.Remove(0, 2);
+            }
+            else
+            {
+                HoraFinal = HoraSinAMPM;
+            }
+        }
+        else
+        {
+
+            if (hora == "12")
+            {
+                HoraFinal = "00" + HoraSinAMPM.Remove(0, 2);
+            }
+            else
+            {
+                HoraFinal = HoraSinAMPM;
+            }
+
+        }
+
+        return HoraFinal;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+       // TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        string result = Result.timeConversion(s);
+
+        //textWriter.WriteLine(result);
+
+        //textWriter.Flush();
+        //textWriter.Close();
+    }
+}
